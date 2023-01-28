a4e800249094a02c485bac7b75860fcf9195afa7
vincegab <vincegab@my.yorku.ca>
2023-01-27 21:37:20 -0500

    01/27/23-adds a testbench package

---
+++ src/pkg1/Test1.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
