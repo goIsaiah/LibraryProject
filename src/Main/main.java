b2a2de1ed6e2e19471e623f92029fecc33385718
amcmi22 <amcmi2@my.yorku.ca>
2023-02-01 12:52:29 -0500

    abbey's first commit

--- src/Main/main.java
+++ src/Main/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Library Project");
+		System.out.println("hello :)");
 	}
 
 }
