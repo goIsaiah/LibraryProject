c0b908dda8e8398d8cdc6ee1589681caa6cdff94
vincegab <vincegab@my.yorku.ca>
2023-02-03 13:17:03 -0500

    makes new branch for iteration-0

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,10 +4,11 @@ import org.json.JSONObject;
 
 //https://www.googleapis.com/books/v1/volumes?q{}
 public class Main {
+	
     public static void main(String[] args) {
     	googleJSON googleAPI = new googleJSON();
-    	String ISBN = "‎‎‎9780451524935";
-    	JSONObject isbnJSON = googleAPI.getJSON(ISBN);
+//    	String ISBN = "‎‎‎9780451524935";
+//    	JSONObject isbnJSON = googleAPI.getJSON(ISBN);
     	
     	String searchKey = "Pokemon";
     	JSONObject searchJSON = googleAPI.getSearch(searchKey);
