67518d5a405e3b487c2b15ed4823a4d098f414a3
Sathira Williams <sathira.williams@gmail.com>
2023-02-08 17:35:51 -0500

    Main components of main page added to UI.

--- src/GUI/MainPage.java
+++ src/GUI/MainPage.java
@@ -2,7 +2,6 @@ package GUI;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-
 import net.miginfocom.swing.MigLayout;
 import java.awt.*;
 import java.net.URL;
@@ -21,7 +20,7 @@ public class MainPage extends JFrame {
         Container container = getContentPane();
         container.setLayout(new BorderLayout());
         JPanel sidebar = new JPanel();
-        sidebar.setPreferredSize(new Dimension(225, 0));
+        sidebar.setPreferredSize(new Dimension(75, 0));
         sidebar.setBackground(Color.decode("#0B6BCC"));
         container.add(sidebar, BorderLayout.WEST);
         
@@ -39,22 +38,51 @@ public class MainPage extends JFrame {
         sidebar.add(hamburger);
         
         //TITLE
-        JPanel content = new JPanel(new MigLayout());
-        content.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        JLabel label = new JLabel("   BookMate");
+//        JPanel content = new JPanel(new MigLayout("gap 0 0", "[grow]", "[grow, center]"));
+//        content.setBorder(BorderFactory.createEmptyBorder(30, 50, 20, 50));
+//        JLabel label = new JLabel("BookMate");
+//        label.setFont(new Font(Font.SANS_SERIF, Font.BOLD,30));
+//        content.add(label, "dock north");
+//        container.add(content, BorderLayout.CENTER);
+//       
+//        for (int i = 0; i < 5; i++) {
+//        	JLabel label3 = new JLabel("    “This is a title of a book “  - “Author” - “Genre”- “ Rating” -”ISBN”(Optional)    ");
+//            Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 1);
+//            label3.setBorder(border);
+//            label3.setFont(new Font(Font.SANS_SERIF, Font.ITALIC,20));
+//            label3.setPreferredSize(new Dimension(600, 50));
+//            content.add(label3, "span, wrap");
+//        }
+        
+        JPanel content = new JPanel(new MigLayout("", "[]30[]", "[]30[]"));
+        content.setBorder(BorderFactory.createEmptyBorder(30, 50, 20, 50));
+        JLabel label = new JLabel("BookMate");
         label.setFont(new Font(Font.SANS_SERIF, Font.BOLD,30));
-        content.add(label, "dock north");
+        content.add(label, "cell 0 0");
         container.add(content, BorderLayout.CENTER);
         
         
-        JLabel label2 = new JLabel("TITLE ");
-        Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 2);
-        label2.setBorder(border);
-        label2.setFont(new Font(Font.SANS_SERIF, Font.BOLD,30));
-        content.add(label2, "span");
+        JTextField field = new JTextField("                                     ");
+        field.setFont(new Font(Font.SANS_SERIF, Font.ITALIC,20));
+        Border border = BorderFactory.createLineBorder(Color.DARK_GRAY, 1);
+        field.setBorder(border);
+        content.add(field, "cell 1 0");
+       
+        for (int i = 1; i < 7; i++) {
+        	JLabel label3 = new JLabel("    “This is a title of a book “  - “Author” - “Genre”- “ Rating” -”ISBN”(Optional)    ");
+        	JLabel label4 = new JLabel("   Add    ");
+        	border = BorderFactory.createLineBorder(Color.DARK_GRAY, 1);
+            label3.setBorder(border);
+            label4.setBorder(border);
+            label3.setFont(new Font(Font.SANS_SERIF, Font.ITALIC,20));
+            label3.setPreferredSize(new Dimension(600, 50));
+            String cell = "cell 0 " + i;
+            String cell2 = "cell 1 " + i;
+            content.add(label3, cell);
+            content.add(label4, cell2);
+        }
         
-        
-//        content.setBackground(Color.BLACK);
+        content.setBackground(Color.white);
         
         //SIZE AND LOCATION
         setSize(1200, 700);
