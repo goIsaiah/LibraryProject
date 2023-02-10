2d5ad19023745b62a4fc035edf2923feeadbdbd7
goIsaiah <goisaiah@outlook.com>
2023-02-10 09:03:32 -0500

    Tried to add sidebar and title to RegisterFrame, didn't work

--- src/GUI/RegisterFrame.java
+++ src/GUI/RegisterFrame.java
@@ -1,10 +1,21 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.net.URL;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
 
 public class RegisterFrame extends JFrame {
 
@@ -30,10 +41,32 @@ public class RegisterFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public RegisterFrame() {
+		setTitle("BookMate");
+        
+        //ICON
+        URL iconUrl = getClass().getResource("/Icon.png");
+        ImageIcon icon = new ImageIcon(iconUrl);
+        setIconImage(icon.getImage());
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1080, 607);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		//SIDEBAR
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        JPanel sidebar = new JPanel();
+        sidebar.setPreferredSize(new Dimension(75, 0));
+        sidebar.setBackground(Color.decode("#0B6BCC"));
+        container.add(sidebar, BorderLayout.WEST);
+        
+        JPanel content = new JPanel(new MigLayout("", "[]30[]", "[]30[]"));
+        content.setBorder(BorderFactory.createEmptyBorder(30, 50, 20, 50));
+        JLabel label = new JLabel("BookMate");
+        label.setFont(new Font(Font.SANS_SERIF, Font.BOLD,30));
+        content.add(label, "cell 0 0");
+        container.add(content, BorderLayout.CENTER);
 
 		setContentPane(contentPane);
 	}
