f5a81c44b952a3b1237ae22c252e837099ed8629
goIsaiah <goisaiah@outlook.com>
2023-02-10 08:48:16 -0500

    Changed dimensions for the RegisterFrame

--- src/GUI/RegisterFrame.java
+++ src/GUI/RegisterFrame.java
@@ -31,7 +31,7 @@ public class RegisterFrame extends JFrame {
 	 */
 	public RegisterFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1080, 607);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
