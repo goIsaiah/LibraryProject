c2f9f0a3eb5716dc429ef07b18647f66ddde9089
goIsaiah <goisaiah@outlook.com>
2023-02-14 20:10:41 -0500

    more changes

--- src/GUI/RegisterFrame.java
+++ src/GUI/RegisterFrame.java
@@ -2,26 +2,22 @@ package GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Image;
 import java.net.URL;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import net.miginfocom.swing.MigLayout;
 
-public class RegisterFrame extends JFrame {
+public class RegisterFrame extends Template {
 
 	private JPanel contentPane;
 
@@ -45,73 +41,34 @@ public class RegisterFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public RegisterFrame() {
-		// WINDOW_NAME
 		setTitle("BookMate");
-		// ICON
-		URL iconUrl = getClass().getResource("/Icon.png");
-		ImageIcon icon = new ImageIcon(iconUrl);
-		setIconImage(icon.getImage());
-		// SIDEBAR
-		Container container = getContentPane();
-		container.setLayout(new BorderLayout());
-		JPanel sidebar = new JPanel();
-		sidebar.setPreferredSize(new Dimension(75, 0));
-		sidebar.setBackground(Color.decode("#0B6BCC"));
-		container.add(sidebar, BorderLayout.WEST);
-		// HAMBURGER_MENU
-		URL hamURL = getClass().getResource("/Ham.png");
-		ImageIcon hamIcon = new ImageIcon(hamURL);
-		Image img = hamIcon.getImage();
-		img = img.getScaledInstance(75, 75, java.awt.Image.SCALE_SMOOTH);
-		hamIcon = new ImageIcon(img);
-		JButton hamburger = new JButton();
-		hamburger.setIcon(hamIcon);
-		hamburger.setPreferredSize(new Dimension(75, 75));
-		hamburger.setBorder(null);
-		hamburger.setContentAreaFilled(false);
-		sidebar.add(hamburger);
-	}
-	
-	public void closeOP() {
-		setSize(1280, 720);
-		setLocationRelativeTo(null);
-		getContentPane().setBackground(Color.white);
+        
+        //ICON
+        URL iconUrl = getClass().getResource("/Icon.png");
+        ImageIcon icon = new ImageIcon(iconUrl);
+        setIconImage(icon.getImage());
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
-	
-	public Component searchBar() {
-		//SEARCH_FIELD
-        JTextField searchField = new JTextField(15);
-        searchField.setPreferredSize(new Dimension(searchField.getPreferredSize().width, 20));
-        searchField.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
-        searchField.setBorder(BorderFactory.createCompoundBorder(
-            BorderFactory.createMatteBorder(0, 0, 1, 0, Color.LIGHT_GRAY),
-            BorderFactory.createEmptyBorder(0, 0, 0, 0) // Add left margin for the text
-        ));
-        searchField.setBackground(new Color(240, 240, 240));
+		setBounds(100, 100, 1080, 607);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		//SIDEBAR
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        JPanel sidebar = new JPanel();
+        sidebar.setPreferredSize(new Dimension(75, 0));
+        sidebar.setBackground(Color.decode("#0B6BCC"));
+        container.add(sidebar, BorderLayout.WEST);
         
-        //SEARCH_ICON
-        URL searchURL = getClass().getResource("/searchIcon.png");
-        ImageIcon searchIcon = new ImageIcon(searchURL);
-        Image img = searchIcon.getImage();
-        img = img.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
-        searchIcon = new ImageIcon(img);
-        JLabel searchIconLabel = new JLabel(searchIcon);
+        JPanel content = new JPanel(new MigLayout("", "[]30[]", "[]30[]"));
+        content.setBorder(BorderFactory.createEmptyBorder(30, 50, 20, 50));
+        JLabel label = new JLabel("BookMate");
+        label.setFont(new Font(Font.SANS_SERIF, Font.BOLD,30));
+        content.add(label, "cell 0 0");
+        container.add(content, BorderLayout.CENTER);
 
-        //SEARCH_PANEL
-        JPanel searchPanel = new JPanel(new MigLayout("", "[]10[]"));
-        searchPanel.add(searchIconLabel,"cell 0 0");
-        searchPanel.add(searchField, "cell 1 0");
-        
-        //SEARCH_BUTTON
-        JButton searchButton = new JButton("Search");
-        searchButton.setBorderPainted(false);
-        searchButton.setContentAreaFilled(false);
-        searchButton.setFocusPainted(false);
-        searchPanel.add(searchButton, "cell 2 0");
-		return searchPanel;
-
+		setContentPane(contentPane);
 	}
 
 }
