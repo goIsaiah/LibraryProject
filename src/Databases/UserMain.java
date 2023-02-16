bfcdfff668c7706fa4fe61fb548b79611b441058
goIsaiah <goisaiah@outlook.com>
2023-02-16 12:57:35 -0500

    Merge branch 'master' of https://github.com/goIsaiah/LibraryProject.git

--- src/Databases/UserMain.java
+++ src/Databases/UserMain.java
@@ -8,11 +8,11 @@ public class UserMain {
 	
 	public static void main(String[] args) {
 
-	User user1 = new User("jondoey124", "qwerty123");
-	User user2 = new User("potterfan7", "dlkklk");
-	User user3 = new User("poloboyw7", "you1240");
-	User user4 = new User("pizzaeater123", "asdf56");
-	User user5 = new User("munchlover2", "pollui12");
+	User user1 = new User("jondoey124", "qwerty123", "jon@my.yorku.ca");
+	User user2 = new User("potterfan7", "dlkklk", "potter@my.yorku.ca");
+	User user3 = new User("poloboyw7", "you1240", "polo@my.yorku.ca");
+	User user4 = new User("pizzaeater123", "asdf56", "pizza@my.yorku.ca");
+	User user5 = new User("munchlover2", "pollui12", "munch@my.yorku.ca");
 	
 	ArrayList <User> users = new ArrayList<User>();
 	
--- src/DomainObjects/User.java
+++ src/DomainObjects/User.java
@@ -13,16 +13,18 @@ public class User
 	private String lastName;
 	private String username;
 	private String password;
+	private String email;
 	private String birthday;
 	private Date dateJoined;
 	private ArrayList<Book> checkedOutBooks;
 	private ArrayList<User> followerList; 
 
 	//creating constructor
-	public User(String username, String password)
+	public User(String username, String password, String email)
 	{
 		this.username = username;
 		this.password = password;
+		this.email = email;
 
 		this.firstName = null;
 		this.lastName = null;
@@ -116,6 +118,14 @@ public class User
 	public void setPassword(String password) 
 	{
 		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getBirthday()
