7ce4d86bfe2703fab2c1d30ce0d08d884bc778c1
brazyboijet <jethro.emilraj@gmail.com>
2023-02-17 09:42:27 -0500

    UserMain changes

--- src/Databases/UserMain.java
+++ src/Databases/UserMain.java
@@ -2,26 +2,32 @@ package Databases;
 
 import java.util.ArrayList;
 
+import DomainObjects.Book;
 import DomainObjects.User;
 
 public class UserMain {
 	
-	public static void main(String[] args) {
-
+	
 	User user1 = new User("jondoey124", "qwerty123", "jon@my.yorku.ca");
 	User user2 = new User("potterfan7", "dlkklk", "potter@my.yorku.ca");
 	User user3 = new User("poloboyw7", "you1240", "polo@my.yorku.ca");
 	User user4 = new User("pizzaeater123", "asdf56", "pizza@my.yorku.ca");
 	User user5 = new User("munchlover2", "pollui12", "munch@my.yorku.ca");
 	
-	ArrayList <User> users = new ArrayList<User>();
+	ArrayList <User> users;
 	
-	users.add(user1);
-	users.add(user2);
-	users.add(user3);
-	users.add(user4);
-	users.add(user5);
-	
-	
+	UserMain(){
+		users =new ArrayList<User>();
+		users.add(user1);
+		users.add(user2);
+		users.add(user3);
+		users.add(user4);
+		users.add(user5);
 	}
+	
+	public ArrayList<User> getList(){
+		return users;
+	}
+	
+	
 }
