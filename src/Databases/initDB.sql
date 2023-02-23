169872fbfea0022f4b0b8421968f7d2a94b6234f
Vince Gabriel Flores <vincegabriel.flores@gmail.com>
2023-02-23 11:32:22 -0500

    adds book table to sql db

--- src/Databases/initDB.sql
+++ src/Databases/initDB.sql
@@ -1,1 +1,7 @@
 Create database bookmateD;
+use bookmateD;PRIMARY
+create table Books(
+bookTitle varchar(255) primary key, 
+Author varchar(255) 
+);
+ 
---
+++ src/sqlDB/DB.java
@@ -0,0 +1,9 @@
+package sqlDB;
+
+public interface DB {
+	public static String url = "jdbc:mysql://localhost:3306/bookmateD"; 
+	public static String user = "root"; 
+	public static String password = ""; 
+	
+	
+}
