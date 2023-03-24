ea861695b6b5796e2bf480ba62d807e25b53a5c6
Sathira <sathira.williams@gmail.com>
2023-03-24 09:26:14 -0400

    Changes to LibUI: enter key functions as button click.

--- src/GUI/LibraryUI.java
+++ src/GUI/LibraryUI.java
@@ -3,8 +3,8 @@ package GUI;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +17,6 @@ public class LibraryUI {
 	
 	public LibraryUI() {
 		password(); 
-//		init();
 	}
 	
 	public static void main(String[] args) {
@@ -26,19 +25,18 @@ public class LibraryUI {
 	
 	private void init() {
 		try {
+			
 			 new Template();
 		} catch (Exception e) {
 		} 
 	}
 	
-	
 	public void password() {
 		JFrame frame = new JFrame(); 
 		JTextField text_field = new JTextField(); 
 		text_field.setColumns(35);
-		JLabel label = new JLabel("Enter Mysql Authentication Password"); 
+		JLabel label = new JLabel("Enter Mysql Authentication Password: "); 
 		JButton submit = new JButton();
-
 		submit.addActionListener(new ActionListener() {
 
 			@Override
@@ -50,22 +48,29 @@ public class LibraryUI {
 			}
 
 		});
-
-		submit.setText("submit");
-
+		
+		text_field.addKeyListener(new KeyAdapter() {
+		    public void keyPressed(KeyEvent e) {
+		        if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+		            sqlpassword = text_field.getText(); 
+		            text_field.setText("");
+		            frame.dispose();
+		            init(); 
+		        }
+		    }
+		});
+		
+		//SQL Popup
+		submit.setText("Submit");
 		frame.setPreferredSize(new Dimension(200 , 200));
 		frame.setSize(new Dimension(300 , 200));
 		frame.setLayout(new MigLayout("", "[]", "[]10[]10[]" ));
-
-
 		frame.add(label, "cell 0 1");
 		frame.add(text_field, "cell 0 2");
 		frame.add(submit, "cell 0 3");
-
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
 	}
 	
 }
