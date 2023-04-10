0988decd2aa5f7d26bb8fe3bb0e28f50ff2b9bd9
Vince Gabriel Flores <vincegabriel.flores@gmail.com>
2023-04-09 20:06:40 -0400

    adds friendlist in profile pages

---
+++ src/Databases/DBFollowers.java
@@ -0,0 +1,36 @@
+package Databases;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+import GUI.LibraryUI;
+
+public class DBFollowers {
+	
+	protected static Connection conn; 
+	
+	public DBFollowers() throws SQLException{
+		conn = LibraryUI.conn; 
+	}
+	
+	
+	public ArrayList<Integer> getAllFriend_id(int user_id) throws SQLException{
+		ArrayList<Integer> list = new ArrayList<>(); 
+		String query = "Select * from FOLLOWERS"; 
+		Statement s = conn.createStatement();
+		ResultSet r = s.executeQuery(query); 
+		
+		while(r.next()) {
+			if(r.getInt(1) == user_id) {
+				list.add(r.getInt(2)); 
+			}
+		}
+		
+		return list;
+	}
+	
+	
+}
--- src/GUI/ProfilePage.java
+++ src/GUI/ProfilePage.java
@@ -6,11 +6,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 import javax.swing.*;
 
+import Databases.DBFollowers;
+import Databases.DBUser;
 import Databases.DBUserInfo;
 import DomainObjects.User;
 import net.miginfocom.swing.MigLayout;
@@ -35,10 +42,16 @@ public class ProfilePage extends JPanel{
         setBackground(Color.white);
         DBUserInfo db = new DBUserInfo();
         currUser = db.getUserInfo(currUser);
+        
         setBookMateLabel(labelString);   // BOOKMATE_LABEL
         setPhoto();   // PROFILE_PICTURE
         setProfileInfo();// USER_INFO
-
+        addFriendsList();
+    }
+    
+    private void addFriendsList() {
+    	JScrollPane friendList = new FriendsList(currUser); 
+    	add(friendList, "cell 0 7");
     }
     
     private void setBookMateLabel(String labelString) {
@@ -57,8 +70,14 @@ public class ProfilePage extends JPanel{
         bioArea.setLineWrap(true);
         bioArea.setWrapStyleWord(true);
         bioArea.setEditable(false);
-        bioArea.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-        add(bioArea, "cell 1 2");
+        bioArea.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+        
+        JScrollPane sp = new JScrollPane();
+        sp.setPreferredSize(new Dimension(900, 100));
+        sp.setViewportView(bioArea);
+        add(sp, "cell 1 2");
+        
+        
     }
     
     
@@ -80,11 +99,105 @@ public class ProfilePage extends JPanel{
          picLabel.setMinimumSize(new Dimension(150, 150)); // set minimum size to prevent distortion
          picLabel.setMaximumSize(new Dimension(150, 150)); // set maximum size to prevent stretching
          add(picLabel, "cell 0 2");
-
     }
 
-
-
-
-
 }
+
+
+
+class FriendsList extends JScrollPane {
+	/*
+	 * TODO get all friend_id of loggedin_iser 
+	 * TODO initialize all the MiniProfiles using the friend_id's
+	 * TODO add event listener 
+	 */
+	private JPanel panel; 
+    private Template parentTemplate;
+    
+	private User user; 
+	public FriendsList(User user) {
+		this.user = user; 
+		panel = makeContentPanel(); 
+		try {
+			addContentToPanel();
+			init(); 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	private void init() {
+		this.setViewportView(panel);
+		this.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		this.setPreferredSize(new Dimension(900, 300));
+	}
+	
+	private JPanel makeContentPanel() {
+		panel = new JPanel(); 
+		panel.setPreferredSize(new Dimension(900, 300));
+		panel.setBorder(BorderFactory.createLineBorder(Color.black));
+		panel.setLayout(new MigLayout("", "[]", "[] [] [] "));
+		return panel; 
+	}
+	
+	private void addContentToPanel() throws SQLException {
+		DBFollowers db_f = new DBFollowers(); 
+		DBUser db = new DBUser(); 
+	   DBUserInfo info = new DBUserInfo();
+	   
+		ArrayList<Integer> ids = db_f.getAllFriend_id(db.getUserId(this.user));
+		//TODO get list of the userinfo
+		Hashtable<Integer , String> set = getInfo(ids);
+	 parentTemplate = (Template)SwingUtilities.getWindowAncestor(this);
+		
+		
+		int size = ids.size(); 
+		for(int i =0 ; i<size ; i++ ) {
+			int id = ids.get(i);
+			String value = set.get(id); 
+			MiniProfile profile = new MiniProfile(value); 
+			profile.setPhoto(info.getPhotoUrl(id));
+            profile.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                	User friend = db.getUser(id);
+                	parentTemplate.userF = friend; 
+                	System.out.println(friend.getUsername());
+//                	parentTemplate.showPanel("fProfile");
+                	parentTemplate.showPanel("OtherProfile");
+                    System.out.println("Yay you clicked me");
+                }
+            });
+            panel.add(profile);
+			
+		}
+	}
+	
+	public Hashtable<Integer, String> getInfo(ArrayList<Integer> keys) throws SQLException{
+		DBUser db = new DBUser(); 
+		Hashtable<Integer , String> allset = db.getUserInfo();
+		Hashtable<Integer , String> set = new Hashtable<>(); 
+		int size = keys.size(); 
+		for(int i = 0 ; i<size ; i++) {
+			int id = keys.get(i);
+			if(allset.containsKey(id)) {
+				set.put(id, allset.get(id));
+			}
+		}
+		
+		
+		return set; 
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
